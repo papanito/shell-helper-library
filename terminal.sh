ed1c64612f13a383c2a891f411dd78a00b92cf00
papanito <papanito@wyssmann.com>
2022-09-19 09:00:12 +0200

    fix: remove terminal.sh

--- terminal.sh
+++
@@ -1,8 +0,0 @@
-# @file terminal.sh
-# @brief Terminal and ssh improvements
-# @description Helper functions to work with terminal and ssh
-
-# @section ssh
-# @description helper functions for ssh
-alias sshall='logwatch --service sshd --range all --detail high --print --archives'
-alias sshtoday='logwatch --service sshd --range today --detail high --print --archives'
